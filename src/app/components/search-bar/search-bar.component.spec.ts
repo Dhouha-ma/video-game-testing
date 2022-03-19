e62c3cdc639f038e4e562a5f3980e80d32ac3c9f
dhouha <dhouha.mansour11@gmail.com>
2022-03-19 13:16:15 +0100

    unit testing

--- src/app/components/search-bar/search-bar.component.spec.ts
+++ src/app/components/search-bar/search-bar.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 import { SearchBarComponent } from './search-bar.component';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let routerSpy = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SearchBarComponent ]
+      declarations: [
+        SearchBarComponent 
+      ],
+      providers: [
+        { provide: Router, useValue: routerSpy }
+      ],
+      imports: [ 
+        FormsModule
+      ]
     })
     .compileComponents();
   });
@@ -22,4 +33,17 @@ describe('SearchBarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('onSubmit', () => {
+    it('should', () => {
+      const testForm = <NgForm> {
+        value: {
+            search: "Hello",
+        }
+    };
+      component.onSubmit(testForm);
+      expect(routerSpy.navigate).toHaveBeenCalledWith([ 'search', 'Hello' ]);
+    });
+  });
+  
 });
